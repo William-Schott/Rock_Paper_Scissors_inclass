1deac89deabaa79c016a542242e49fc90f5e411e
William-Schott <97321981+William-Schott@users.noreply.github.com>
2022-01-31 17:38:39 -0500

    Random Computer Choice

--- game.py
+++ game.py
@@ -5,3 +5,31 @@
 # todo: write some Python code here to satisfy the exercise objectives
 # ... https://github.com/prof-rossetti/intro-to-python/blob/main/exercises/rock-paper-scissors/README.md
 #
+# ask for user input
+u = input("Please choose one: 'Rock', 'Paper', 'Scissors?'")
+print("USER CHOSE:", u)
+
+#Validations
+
+
+
+#computer choice
+# Computers choice is defined by variable c
+import random
+options = ["Rock", "Paper", "Scissors"]
+c = random.choice(options)
+print("Computer Chose:",c)
+
+
+
+
+
+
+#Determine the winner
+
+
+
+
+
+
+#final results
