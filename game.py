a69490b41f86a22ac8c96aaeb36e54a451a4d188
William-Schott <97321981+William-Schott@users.noreply.github.com>
2022-02-03 11:05:04 -0500

    Wrote the determineing the winner code

--- game.py
+++ game.py
@@ -6,10 +6,17 @@
 # ... https://github.com/prof-rossetti/intro-to-python/blob/main/exercises/rock-paper-scissors/README.md
 #
 # ask for user input
-u = input("Please choose one: 'Rock', 'Paper', 'Scissors?'")
+
+u = input("Please choose one: 'Rock','Paper', 'Scissors','?".lower().title())
 print("USER CHOSE:", u)
+print("Rock, Paper, Scissors, Shoot!")
+     
+
+
+
 
 #Validations
+
 
 
 
@@ -21,11 +28,24 @@ c = random.choice(options)
 print("Computer Chose:",c)
 
 
-
-
-
-
 #Determine the winner
+if u.lower().title() == c.lower().title() :
+    print("You both played",c,"Its a tie!")
+elif u.lower().title()  == "Rock".lower().title() :
+    if c.lower().title()  == "Paper".lower().title() :
+        print("Paper beats rock, you lose.")
+    else:
+        print("Rock beats scissors, you win!")
+elif u.lower().title()  == "Paper".lower().title() :
+    if c.lower().title()  == "Scissors".lower().title() :
+        print("Scissors beats paper, you lose.")
+    else:
+        print("Paper beats rock, you win")
+elif u.lower().title()  == "Scissors".lower().title() :
+    if c.lower().title()  == "Rock".lower().title() :
+        print("Rock beats scissors, you lose.")
+    else:
+        print("Scissors beats paper, you win!")
 
 
 
